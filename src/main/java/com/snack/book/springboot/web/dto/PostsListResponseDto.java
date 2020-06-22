674835a914b8f1da06ce8facd05f33a472109752
ysjune <windsyou@gmail.com>
2020-06-22 23:35:07 +0900

    chapter 5 중간 커밋

---
+++ src/main/java/com/snack/book/springboot/web/dto/PostsListResponseDto.java
@@ -0,0 +1,21 @@
+package com.snack.book.springboot.web.dto;
+
+import com.snack.book.springboot.domain.posts.Posts;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class PostsListResponseDto {
+    private Long id;
+    private String title;
+    private String author;
+    private LocalDateTime modifiedDate;
+
+    public PostsListResponseDto(Posts posts) {
+        this.id = posts.getId();
+        this.title = posts.getTitle();
+        this.author = posts.getAuthor();
+        this.modifiedDate = posts.getModifiedDate();
+    }
+}
