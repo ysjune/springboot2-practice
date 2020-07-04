f30987087bb02c0e5f71d7f0baec245c90d57d13
ysjune <windsyou@gmail.com>
2020-07-05 01:33:46 +0900

    deploy.sh 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-REPOSITORY=/home/ec-user/app/step2
-PROJECT_NAME=springboot2/webservice
+REPOSITORY=/home/ec2-user/app/step2
+PROJECT_NAME=springboot2-webservice
 
 echo "> Build 파일 복사"
 
